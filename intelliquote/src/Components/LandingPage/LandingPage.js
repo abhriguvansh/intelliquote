da08445ccc476684036e745e51d39e01ee40c1f9
Aman <abhriguvansh99@gmail.com>
2020-10-23 11:22:53 -0700

    finished navbar

--- intelliquote/src/Components/LandingPage/LandingPage.js
+++ intelliquote/src/Components/LandingPage/LandingPage.js
@@ -4,9 +4,8 @@ import Navbar from '../Navbar/Navbar'
 import {
   Link
 
-} from 'react-router-dom';
-
-function LandingPage(){
+} from 'react-router-dom'
+function LandingP age(){
     return(
         <div>
         <Navbar />
