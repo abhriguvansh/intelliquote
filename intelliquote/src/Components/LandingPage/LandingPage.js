d256e284bab12a4038dd99b0a21539fa4f26eaa6
Aman <abhriguvansh99@gmail.com>
2020-10-23 12:21:03 -0700

    created questions.js which stores questions that the quiz will display

--- intelliquote/src/Components/LandingPage/LandingPage.js
+++ intelliquote/src/Components/LandingPage/LandingPage.js
@@ -5,7 +5,7 @@ import {
   Link
 
 } from 'react-router-dom'
-function LandingP age(){
+function LandingPage(){
     return(
         <div>
         <Navbar />
---
+++ intelliquote/src/Components/Quiz/questions.js
@@ -0,0 +1,105 @@
+/*
+This file stores the questions that the quiz displays
+Each element in the questions array holds a question and an answer
+The answer is made up of an array of possible choices
+Each element in the answer array keeps track of the personality type associated with the choice:
+
+O = Openness
+C = Conscientiousness
+E = Extroversion 
+A = Agreeableness
+N = Neuroticism 
+
+
+*/
+
+
+
+let questions = [
+
+    {
+        question: 'I Am the life of the party.',
+        answer: [
+            {
+                type: 'E',
+                content: 'Agree'
+            },
+            {
+                type: '',
+                content: 'Neutral'
+            },
+            {
+                type: '',
+                content: 'Disagree'       
+            }
+        ]
+    },
+    {
+        question: 'I am interested in people.',
+        answer: [
+            {
+                type: 'A',
+                content: 'Agree'
+            },
+            {
+                type: '',
+                content: 'Neutral'
+            },
+            {
+                type: '',
+                content: 'Disagree'       
+            }
+        ]
+    },
+    {
+        question: 'I am always prepared',
+        answer: [
+            {
+                type: 'C',
+                content: 'Agree'
+            },
+            {
+                type: '',
+                content: 'Neutral'
+            },
+            {
+                type: '',
+                content: 'Disagree'       
+            }
+        ]
+    },
+    {
+        question: 'I am relaxed most of the time',
+        answer: [
+            {
+                type: 'N',
+                content: 'Agree'
+            },
+            {
+                type: '',
+                content: 'Neutral'
+            },
+            {
+                type: '',
+                content: 'Disagree'       
+            }
+        ]
+    },
+    {
+        question: 'Have a vivid imagination',
+        answer: [
+            {
+                type: 'A',
+                content: 'Agree'
+            },
+            {
+                type: '',
+                content: 'Neutral'
+            },
+            {
+                type: '',
+                content: 'Disagree'       
+            }
+        ]
+    },
+]
