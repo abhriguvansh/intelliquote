a4462338d14ea0cf281b07a7490fbe089fb2b2b7
Aman <abhriguvansh99@gmail.com>
2020-10-24 14:45:02 -0700

    added test in Results.js to test if props are passed properly

--- intelliquote/src/Components/QuizPage/Results/Results.test.js
+++ intelliquote/src/Components/QuizPage/Results/Results.test.js
@@ -1,14 +1,19 @@
 
 import React from 'react';
 import {shallow} from 'enzyme'
-import Result from './Result'
+import Results from './Results'
+import { ExpansionPanelActions } from '@material-ui/core';
 
 //tests that result is rendered without crashing
 it('renders without crashing', () => {
-  shallow(<Result />);
+  shallow(<Results />);
 });
 
 //
+it('test props of Result', () => {
+    const result = shallow(<Results quizResult = 'TEST' />)
+    expect(result.text()).toEqual('You prefer TEST')
+})
 
 
 
