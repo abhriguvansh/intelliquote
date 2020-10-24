af6e94f56f857402a653a1a6effed86e9275f12c
Aman <abhriguvansh99@gmail.com>
2020-10-24 14:36:18 -0700

    added test in Results.js to test if the page is rendered

---
+++ intelliquote/src/Components/QuizPage/Results/Results.test.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import {shallow} from 'enzyme'
+import Result from './Result'
+
+//tests that result is rendered without crashing
+it('renders without crashing', () => {
+  shallow(<Result />);
+});
+
+//
+
+
+
