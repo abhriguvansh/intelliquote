555d9ab5d8852548d04f67a6ddcc75b0858ebd8c
Aman <abhriguvansh99@gmail.com>
2020-10-24 17:19:55 -0700

    added test in QuizPage.test.js to test that the page is rendered without crashing

--- intelliquote/src/Components/QuizPage/QuizPage.test.js
+++ intelliquote/src/Components/QuizPage/QuizPage.test.js
@@ -2,13 +2,17 @@
 import React from 'react';
 import {shallow} from 'enzyme'
 import QuizPage from './QuizPage'
+import {createMemoryHistory} from 'history'
+import LandingPage from '../LandingPage/LandingPage'
+import { Router } from 'react-router-dom'
+import userEvent from '@testing-library/user-event'
+import { render, screen } from '@testing-library/react';
 
 //tests that quiz page is rendered without crashing
 it('renders without crashing', () => {
   shallow(<QuizPage />);
 });
 
-//generate left click action and use the quiz button on the navbar to test it works
 it('test quiz button on navbar', () => {
   const history = createMemoryHistory()
   render(
@@ -18,10 +22,18 @@ it('test quiz button on navbar', () => {
   )
 
   const leftClick = { button: 0 }
-  userEvent.click(screen.getByText(/lll/i), leftClick)
+  userEvent.click(screen.getByText(/lll/i))
 
-  // check that the content changed to the new page 'I am the lift of the party' is one of the first things the user sees on the quiz page
-  expect(screen.getByText(/I am relaxed most of the time/i)).toBeInTheDocument()
+
+  expect(screen.findByText('Question 2'))
+  userEvent.click(screen.getByText(/lll/i))
+
+
+  expect(screen.findByText('Question 2'))
+  userEvent.click(screen.getByText(/lll/i))
+
+
+  expect(screen.findByText('Question 2'))
 });
 
 
