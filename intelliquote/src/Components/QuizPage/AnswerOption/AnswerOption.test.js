93f7ca7f0deb3c43ae86576600f315a7d49bb618
Aman <abhriguvansh99@gmail.com>
2020-10-24 17:07:33 -0700

    added test in AnswerOptions.test.js to test that props are passed properly

--- intelliquote/src/Components/QuizPage/AnswerOption/AnswerOption.test.js
+++ intelliquote/src/Components/QuizPage/AnswerOption/AnswerOption.test.js
@@ -6,16 +6,16 @@ import AnswerOption from './AnswerOption'
 //tests that result is rendered without crashing
 it('renders without crashing', () => {
     function test(){
-        console.log('')
+        //
     }
   shallow(<AnswerOption answerType = 'A' answerContent = 'click here' answer = '' onAnswerSelected = {test()}   />);
 });
 
-// //test that props are passed properly
-// it('test props of QuizCount', () => {
-//     const result = shallow(<QuestionCount counter = {1} total = {2} />)
-//     expect(result.text()).toEqual('Question 1 of 2')
-// })
+//test that props are passed properly
+it('test props of AnswerOption', () => {
+    const result = shallow(<AnswerOption answerType = 'A' answerContent = 'click here' answer = ' ' onAnswerSelected = {test}   />);
+    expect(result.text()).toEqual('click here')
+})
 
 
 
--- intelliquote/src/Components/QuizPage/QuizPage.test.js
+++ intelliquote/src/Components/QuizPage/QuizPage.test.js
@@ -8,5 +8,20 @@ it('renders without crashing', () => {
   shallow(<QuizPage />);
 });
 
+//generate left click action and use the quiz button on the navbar to test it works
+it('test quiz button on navbar', () => {
+  const history = createMemoryHistory()
+  render(
+    <Router history = {history}>
+      <QuizPage />
+    </Router>
+  )
+
+  const leftClick = { button: 0 }
+  userEvent.click(screen.getByText(/lll/i), leftClick)
+
+  // check that the content changed to the new page 'I am the lift of the party' is one of the first things the user sees on the quiz page
+  expect(screen.getByText(/I am relaxed most of the time/i)).toBeInTheDocument()
+});
 
 
