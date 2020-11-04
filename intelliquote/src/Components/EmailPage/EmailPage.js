03770f30f4e9ed37174cbe9518a552b5c724d3ff
Aman <abhriguvansh99@gmail.com>
2020-11-04 14:33:30 -0800

    fixed some test files

--- intelliquote/src/Components/EmailPage/EmailPage.js
+++ intelliquote/src/Components/EmailPage/EmailPage.js
@@ -1,34 +1,47 @@
-import React from 'react'
-import './EmailPage.css'
-import Navbar from '../Navbar/Navbar'
+import React from 'react';
+import './EmailPage.css';
+import Navbar from '../Navbar/Navbar';
 
-function EmailPage(){
-    return(
-        <div>
-        <Navbar />
-          <div className='container'>
-              <label className='email'>
-                <p>Email</p>
-              </label>
-              <br />
-              <div className='email-div'>
-                <input
-                  type='text'
-                  placeholder='Enter Email'
-                  className='email-input'
-                  required
-                />
-              </div>
-              <div className='submit-div'>
-                {' '}
-                <button type='submit' className='submit'>
-                  submit
-                </button>
-              </div>
-            </div>
+function EmailPage() {
+  return (
+    <div>
+      <Navbar />
+      <div className='container'>
+        <label className='email'>
+          <p>Email</p>
+        </label>
+        <br />
+        <div className='email-div'>
+          <input
+            type='email'
+            placeholder='Enter Email'
+            className='email-input'
+            required
+          />
         </div>
-    )
+        <div className='interval-div'>
+          <label htmlFor='daily'>
+            Daily <input type='radio' name='interval' id='daily' />
+          </label>
 
+          <label htmlFor='weekly'>
+            Weekly
+            <input type='radio' name='interval' id='weekly' />
+          </label>
+
+          <label htmlFor='monthly'>
+            Monthly <input type='radio' name='interval' id='monthly' />
+          </label>
+        </div>
+        <div className='submit-div'>
+          {' '}
+          <button type='submit' className='submit'>
+            submit
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default EmailPage
+export default EmailPage;
--- intelliquote/src/Components/LandingPage/LandingPage.test.js
+++ intelliquote/src/Components/LandingPage/LandingPage.test.js
@@ -1,23 +1,19 @@
-
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import {shallow} from 'enzyme'
-import {createMemoryHistory} from 'history'
-import { Router } from 'react-router-dom'
-import userEvent from '@testing-library/user-event'
-import '@testing-library/jest-dom/extend-expect'
-import LandingPage from './LandingPage'
+import { shallow } from 'enzyme';
+import { createMemoryHistory } from 'history';
+import { Router } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom/extend-expect';
+import LandingPage from './LandingPage';
 
 //tests that landing page is rendered
 it('render landing page', () => {
-  const history = createMemoryHistory()
+  const history = createMemoryHistory();
   render(
-    <Router history = {history}>
+    <Router history={history}>
       <LandingPage />
     </Router>
-  )
-  expect(screen.getByText(/LandingPage/i)).toBeInTheDocument()
-  });
-
-
-
+  );
+  expect(screen.getByText(/LandingPage/i)).toBeInTheDocument();
+});
