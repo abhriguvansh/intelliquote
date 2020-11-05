0bb335a2bc9bff94db28c6b6acab1b86c175909b
Aman <abhriguvansh99@gmail.com>
2020-11-04 20:48:28 -0800

    added personality type for email form

--- intelliquote/src/Components/EmailPage/EmailPage.js
+++ intelliquote/src/Components/EmailPage/EmailPage.js
@@ -1,50 +1,77 @@
 import React from 'react';
+import { Component } from 'react';
 import './EmailPage.css';
 import Navbar from '../Navbar/Navbar';
 
-function EmailPage() {
-  return (
-    <div>
-      <Navbar />
-      <div className='container'>
-        <label className='email'>
-          <p>Email</p>
-        </label>
-        <br />
-        <div className='email-div'>
-          <input
-            type='email'
-            placeholder='Enter Email'
-            className='email-input'
-            required
-          />
-        </div>
-        <div className='interval-div'>
-          <label htmlFor='daily'>
-            Daily <input type='radio' name='interval' id='daily' />
+class EmailPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <div className='container'>
+          <label className='email'>
+            <p>Email</p>
           </label>
+          <br />
+          <div className='email-div'>
+            <input
+              type='email'
+              placeholder='Enter Email'
+              className='email-input'
+              required
+            />
+          </div>
+          <div className='interval-div'>
+            <label htmlFor='daily'>
+              Daily <input type='radio' name='interval' id='daily' />
+            </label>
 
-          <label htmlFor='weekly'>
-            Weekly
-            <input type='radio' name='interval' id='weekly' />
-          </label>
+            <label htmlFor='weekly'>
+              Weekly
+              <input type='radio' name='interval' id='weekly' />
+            </label>
 
-          <label htmlFor='monthly'>
-            Monthly <input type='radio' name='interval' id='monthly' />
-          </label>
-        </div>
-        <div className='button-div'>
-          {' '}
-          <button type='submit' className='submit'>
-            Subscribe
-          </button>
-          <button type='submit' className='submit'>
-            Unsubscribe
-          </button>
+            <label htmlFor='monthly'>
+              Monthly <input type='radio' name='interval' id='monthly' />
+            </label>
+          </div>
+
+          <div className='personality-div'>
+            <label htmlFor='openness'>
+              openness <input type='radio' name='personality' id='openness' />
+            </label>
+
+            <label htmlFor='conscientiousness'>
+              conscientiousness
+              <input type='radio' name='personality' id='conscientiousness' />
+            </label>
+
+            <label htmlFor='extroversion'>
+              extroversion{' '}
+              <input type='radio' name='personality' id='extroversion' />
+            </label>
+            <label htmlFor='agreeableness'>
+              agreeableness{' '}
+              <input type='radio' name='personality' id='agreeableness' />
+            </label>
+            <label htmlFor='neuroticism'>
+              neuroticism{' '}
+              <input type='radio' name='personality' id='neuroticism' />
+            </label>
+          </div>
+          <div className='button-div'>
+            {' '}
+            <button type='submit' className='submit'>
+              Subscribe
+            </button>
+            <button type='submit' className='submit'>
+              Unsubscribe
+            </button>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default EmailPage;
