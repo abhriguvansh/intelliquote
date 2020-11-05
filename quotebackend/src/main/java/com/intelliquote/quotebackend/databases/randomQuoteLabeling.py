6dad48a4dbc2194b42cad941c1810825a55e3673
cxz41 <jackzhang9100@yahoo.com>
2020-11-05 02:23:11 -0500

    writing to labeled txt

--- quotebackend/src/main/java/com/intelliquote/quotebackend/databases/randomQuoteLabeling.py
+++ quotebackend/src/main/java/com/intelliquote/quotebackend/databases/randomQuoteLabeling.py
@@ -3,9 +3,14 @@ from collections import defaultdict
 file = open("quotebackend\src\main\java\com\intelliquote\quotebackend\databases\quotes.txt", 'r')
 d = {0 : 'C', 1 : 'N', 2 : 'A', 3 : 'E', 4 : 'O'}
 c = defaultdict(int)
+
+output = open("quotebackend\src\main\java\com\intelliquote\quotebackend\databases\quotes_labeled.txt", 'w')
 for line in file:
     rnd = random.randint(0,4)
     write_line = line[0:len(line)-2] + " | " + d[rnd]
     c[d[rnd]] += 1
-    print(str(write_line))
+    ret = output.write(str(write_line))
+    print(ret)
+
+output.close
 print(c)
