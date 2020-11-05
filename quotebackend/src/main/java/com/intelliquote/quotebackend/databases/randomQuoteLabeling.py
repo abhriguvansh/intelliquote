2db7da03ffcb4a72008c7346dfdc373a5170de64
cxz41 <jackzhang9100@yahoo.com>
2020-11-05 01:49:12 -0500

    generator + dictionary

--- quotebackend/src/main/java/com/intelliquote/quotebackend/databases/randomQuoteLabeling.py
+++ quotebackend/src/main/java/com/intelliquote/quotebackend/databases/randomQuoteLabeling.py
@@ -1,3 +1,8 @@
+import random
 file = open("quotebackend\src\main\java\com\intelliquote\quotebackend\databases\quotes.txt", 'r')
+d = {0 : 'C', 1 : 'N', 2 : 'A', 3 : 'E', 4 : 'O'}
 for line in file:
-    print(line)
+    rnd = random.randint(0,4)
+    write_line = line[0:len(line)-2] + " | " + d[rnd]
+    print(str(write_line))
+    
