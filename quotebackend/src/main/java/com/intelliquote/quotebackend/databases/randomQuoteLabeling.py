6d0557a740195e70fafd0ef0c62818a3f80e1d61
cxz41 <jackzhang9100@yahoo.com>
2020-11-08 11:30:00 -0500

    fix script

--- quotebackend/src/main/java/com/intelliquote/quotebackend/databases/randomQuoteLabeling.py
+++ quotebackend/src/main/java/com/intelliquote/quotebackend/databases/randomQuoteLabeling.py
@@ -7,7 +7,7 @@ c = defaultdict(int)
 output = open("quotebackend\src\main\java\com\intelliquote\quotebackend\databases\quotes_labeled.txt", 'w')
 for line in file:
     rnd = random.randint(0,4)
-    write_line = line[0:len(line)-2] + " | " + d[rnd]
+    write_line = line.replace('\n', '') + " | " + d[rnd]
     c[d[rnd]] += 1
     ret = output.write(str(write_line) + '\n')
     print(ret)
