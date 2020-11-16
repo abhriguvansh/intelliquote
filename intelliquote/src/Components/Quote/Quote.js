6d994bb1e49b711c23ef393f7a0a8dbf6f332de3
Aman <abhriguvansh99@gmail.com>
2020-11-15 18:14:49 -0800

    added button for flagging quotes

--- intelliquote/src/Components/Quote/Quote.js
+++ intelliquote/src/Components/Quote/Quote.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Quote.css';
 
+function handleClick() {}
 export default function Quote({ quotes }) {
   if (!quotes) {
     return quotes;
@@ -13,6 +14,7 @@ export default function Quote({ quotes }) {
           <div key={quote.id} className={'quote-div'}>
             <h1 className={'quote'}>{quote.quoteContent}</h1>
             <h2 className={'author'}>{quote.author}</h2>
+            <button onClick={handleClick}>Flag Quote</button>
           </div>
         ))}
     </div>
